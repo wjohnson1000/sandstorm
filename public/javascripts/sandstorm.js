d4c07548af2c7217ba5fa8dc881e48ecb48397cd
Lou Fontana <loufontana@gmail.com>
2016-01-29 15:50:38 -0700

    Added audio and animation

--- public/javascripts/sandstorm.js
+++ public/javascripts/sandstorm.js
@@ -6,18 +6,21 @@ sand.controller('storm', ['$scope', function($scope){
 
 $scope.allsand = [];
 
+$scope.rave1=false;
+
 $scope.sendsand = function(){
   //if($scope.sandchat.$valid){
     console.log($scope.sandscript);
     sandsocket.emit('chat message', $scope.sandscript);
     $scope.sandscript = '';
- // }; 
+    $scope.rave1=!$scope.rave1;
+ // };
 };
   sandsocket.on('chat message', function(message){
     console.log('received on client');
     $scope.allsand.push(message);
     $scope.$apply();
-    
+
   });
-  
+
 }]);
