02f55ab3c5e817c391be3e3ff45858753aa1347e
William Johnson <wjohnson1000@gmail.com>
2016-01-29 14:23:23 -0700

    chat working

--- server/public/javascripts/sandstorm.js
+++ server/public/javascripts/sandstorm.js
@@ -1,5 +1,23 @@
-var sand = angular.module('sandstormsuperfans', ['ngSockets']);
+var sand = angular.module('sand', []);
 
-sand.controller('storm', ['$scope', function(s){
+var sandsocket = io();
+
+sand.controller('storm', ['$scope', function($scope){
+
+$scope.allsand = [];
+
+$scope.sendsand = function(){
+  //if($scope.sandchat.$valid){
+    console.log($scope.sandscript);
+    sandsocket.emit('chat message', $scope.sandscript);
+    $scope.sandscript = '';
+ // }; 
+};
+  sandsocket.on('chat message', function(message){
+    console.log('received on client');
+    $scope.allsand.push(message);
+    $scope.$apply();
+    
+  });
   
 }]);
